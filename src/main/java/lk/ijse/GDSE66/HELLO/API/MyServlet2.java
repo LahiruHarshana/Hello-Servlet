3141c194d82df0ce8d0a6410c7db0b60051aaca4
LahiruHarshana <lharshana2002@gmail.com>
2023-12-01 15:09:53 +0530

    updated

--- src/main/java/lk/ijse/GDSE66/HELLO/API/MyServlet2.java
+++ src/main/java/lk/ijse/GDSE66/HELLO/API/MyServlet2.java
@@ -1,8 +1,16 @@
 package lk.ijse.GDSE66.HELLO.API;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MyServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("My Servlet 2");
+        resp.getWriter().println("<h1>My Servlet 2</h1>");
 
-
+    }
 }
