8c480b3a1fa58636793afd652859546770ebcfae
LahiruHarshana <lharshana2002@gmail.com>
2023-12-01 13:50:19 +0530

    updated

--- src/main/java/lk/ijse/GDSE66/HELLO/API/HelloServlet.java
+++ src/main/java/lk/ijse/GDSE66/HELLO/API/HelloServlet.java
@@ -1,10 +1,10 @@
 package lk.ijse.GDSE66.HELLO.API;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
