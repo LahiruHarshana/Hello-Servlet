d1e08914def2d61fcf81d5643bec24f3d92ec429
LahiruHarshana <lharshana2002@gmail.com>
2023-12-01 13:27:29 +0530

    updated

--- src/main/java/com/example/helloservlet/HelloServlet.java
+++ src/main/java/com/example/helloservlet/HelloServlet.java
@@ -2,8 +2,13 @@ package com.example.helloservlet;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet() method is invoked");
+        resp.getWriter().println("MyServlet generate the response");
+    }
 }
